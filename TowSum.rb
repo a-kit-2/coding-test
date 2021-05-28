21c26bd4db8eabb61f12c5939dffde3269db7cd0
AkitoNakamura <akito.nakamura.c@gmail.com>
2021-05-29 01:41:41 +0900

    回答

--- TowSum.rb
+++ TowSum.rb
@@ -1,9 +1,18 @@
 # 整数numsの配列と整数targetが与えられた場合、
-# 2つの数値のインデックスを返し、合計してtargetになります。
+# 合計してtargetになる2つの数値のインデックスを返します。
 # 各入力には正確に1つの解があると想定でき、同じ要素を2回使用することはできません。 
 # 回答は任意の順序で返すことができます。
-def two_sum(nums, target)
 
+def two_sum(nums, target)
+  result = []
+  for i in 0...nums.length do
+    for j in (i + 1)...nums.length do
+      if nums[i] + nums[j] == target
+        result.push(i,j)
+      end
+    end
+  end
+  return result
 end
 
-two_sum()
+puts two_sum([2,7,11,15],9)
