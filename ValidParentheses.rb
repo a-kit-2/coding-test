03cb563b3119f7dac593024e788631b14f7e1684
a-kit-2 <akito.nakamura.c@gmail.com>
2021-06-02 22:19:35 +0900

    ValidParentheses

--- ValidParentheses.rb
+++ ValidParentheses.rb
@@ -1,4 +1,4 @@
-# 文字 '('、')'、'{'、'}'、'[' および ']' のみを含む文字列 s を指定して、
+# 文字 '('、')'、'{'、'}'、'[' および ']' のみを含む文字列sを指定して、
 # 入力文字列が有効かどうかを判断します。 
 # 入力文字列は次の場合に有効です: 
 # 1.括弧は同じタイプの括弧で閉じなければなりません。 
@@ -7,7 +7,24 @@
 # Output: true
 
 def is_valid(s)
-  
+  temp = []
+  for i in 0...s.length do
+    if temp[-1] == "(" && s[i] == ")"
+      temp.pop
+    elsif temp[-1] == "[" && s[i] == "]"
+      temp.pop
+    elsif temp[-1] == "{" && s[i] == "}"
+      temp.pop
+    else
+      temp.push(s[i])
+    end
+  end
+  return temp.length <= 0 ? true : false
 end
 
 puts is_valid("()")
+
+
+
+
+
