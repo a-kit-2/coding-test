50020228c796c3f83127da63619d249f00e7777e
a-kit-2 <akito.nakamura.c@gmail.com>
2021-06-06 13:07:38 +0900

    準備

---
+++ ImplementStrStr().rb
@@ -0,0 +1,14 @@
+# haystack 内で needle が最初に出現するインデックスを返します。
+# needle が haystack 一部でない場合は -1 を返します。 
+# 明確化: 
+# needle が空の文字列の場合、何を返す必要がありますか? 
+# この問題のために、needle が空の文字列の場合は 0 を返します。 
+# これは、C の strstr() および Java の indexOf() と一致しています。
+# Input: haystack = "hello", needle = "ll"
+# Output: 2
+
+def str_str(haystack, needle)
+  
+end
+
+puts str_str("hello", "ll")
