15b0c8e409bc221afdad8dd3d0a24c5e170a136a
a-kit-2 <akito.nakamura.c@gmail.com>
2021-06-07 11:37:42 +0900

    SearchInsertPosition

--- SearchInsertPosition.rb
+++ SearchInsertPosition.rb
@@ -6,7 +6,19 @@
 # Output: 2
 
 def search_insert(nums, target)
-  
+  left = 0
+  right = nums.length - 1
+  while left <= right
+    middle = (left + right) / 2
+    if nums[middle] == target
+      return middle
+    elsif nums[middle] < target
+      left = middle + 1
+    else
+      right = middle - 1
+    end
+  end
+  return left
 end
 
 puts search_insert([1,3,5,6], 5)
