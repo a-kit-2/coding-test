aaecc89e72f536003c065a6c22dee9918437330b
a-kit-2 <akito.nakamura.c@gmail.com>
2021-06-08 17:52:05 +0900

    準備

---
+++ MaximumSubarray.rb
@@ -0,0 +1,11 @@
+# 整数配列 nums を指定して、最大の合計を持つ連続したサブ配列 
+# (少なくとも 1 つの数字を含む) を見つけ、その合計を返します。
+# Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+# Output: 6
+# Explanation: [4,-1,2,1] has the largest sum = 6.
+
+def max_sub_array(nums)
+    
+end
+
+puts max_sub_array([-2,1,-3,4,-1,2,1,-5,4])
