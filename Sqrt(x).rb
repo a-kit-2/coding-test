c32951ea39d3c59989d93fd102049198aebe6d88
a-kit-2 <akito.nakamura.c@gmail.com>
2021-06-12 19:49:15 +0900

    準備

---
+++ Sqrt(x).rb
@@ -0,0 +1,15 @@
+# 負でない整数xが与えられた場合、xの平方根を計算して返します。
+# 返されるタイプは整数であるため、10進数は切り捨てられ、
+# 結果の整数部分のみが返されます。 
+# 注: pow（x、0.5）やx ** 0.5などの組み込みの指数関数や
+#     演算子を使用することは許可されていません。
+# Input: x = 8
+# Output: 2
+# Explanation: The square root of 8 is 2.82842..., 
+#              and since the decimal part is truncated, 2 is returned.
+
+def my_sqrt(x)
+  
+end
+
+puts my_sqrt(4)
--- test/q3.rb
+++
@@ -1,4 +0,0 @@
-beef = ARGV[0].to_i
-beef = (beef/500) * 500 
-
-puts beef
