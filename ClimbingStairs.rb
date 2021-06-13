b91129efdcf3d20dc5b7916e2bbaf61700f3cb60
a-kit-2 <akito.nakamura.c@gmail.com>
2021-06-13 21:17:32 +0900

    ClimbingStairs

--- ClimbingStairs.rb
+++ ClimbingStairs.rb
@@ -9,8 +9,17 @@
 #              2. 1 step + 2 steps
 #              3. 2 steps + 1 step
 
+# 0 1 1 2 3 5 8 13
+
 def climb_stairs(n)
-  
+  num1 = 0
+  num2 = 1
+  for i in 0...n do
+    num3 = num1 + num2
+    num1 = num2
+    num2 = num3
+  end
+  return num3
 end
 
 puts climb_stairs(3)
