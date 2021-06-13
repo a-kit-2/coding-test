48671bd4254a5ecf32ff6cd8babce731e8ca50ee
a-kit-2 <akito.nakamura.c@gmail.com>
2021-06-13 19:22:15 +0900

    準備

---
+++ ClimbingStairs.rb
@@ -0,0 +1,16 @@
+# あなたは階段を上っています。 
+# トップに到達するにはnステップかかります。 
+# 毎回、1段または2段登ることができます。 
+# いくつの明確な方法でトップに登ることができますか？
+# Input: n = 3
+# Output: 3
+# Explanation: There are three ways to climb to the top.
+#              1. 1 step + 1 step + 1 step
+#              2. 1 step + 2 steps
+#              3. 2 steps + 1 step
+
+def climb_stairs(n)
+  
+end
+
+puts climb_stairs(3)
