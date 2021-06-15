c29b1978a8cebe2a830fc8073c82d0252bc85b62
a-kit-2 <akito.nakamura.c@gmail.com>
2021-06-15 21:03:17 +0900

    準備

--- MergeSortedArray.rb
+++ MergeSortedArray.rb
@@ -12,7 +12,7 @@
 #              The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
 
 def merge(nums1, m, nums2, n)
-    
+  
 end
 
 puts merge([1,2,3,0,0,0], 3, [2,5,6], 3)
