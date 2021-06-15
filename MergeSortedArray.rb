6f2ac73d03ef7dc279cf3600d1246f4da2ac5a72
a-kit-2 <akito.nakamura.c@gmail.com>
2021-06-15 21:19:48 +0900

    MergeSortedArray

--- MergeSortedArray.rb
+++ MergeSortedArray.rb
@@ -12,7 +12,16 @@
 #              The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
 
 def merge(nums1, m, nums2, n)
-  
+  while n > 0
+    if m == 0 || nums1[m - 1] < nums2[n - 1]
+      nums1[m + n - 1] = nums2[n - 1]
+      n -= 1
+    else
+      nums1[m + n - 1] = nums1[m - 1]
+      m -= 1
+    end
+  end
+  return nums1
 end
 
 puts merge([1,2,3,0,0,0], 3, [2,5,6], 3)
