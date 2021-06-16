365cff67bcf022a78c06ea5f49f39c46feee8447
a-kit-2 <akito.nakamura.c@gmail.com>
2021-06-16 22:59:52 +0900

    BinaryTreeInoderTraversal

--- BinaryTreeInorderTraversal.rb
+++ BinaryTreeInorderTraversal.rb
@@ -23,13 +23,24 @@ class TreeNode
 end
 
 n1 = TreeNode.new(1)
-n2 = TreeNode.new(2)
-n3 = TreeNode.new(3)
-n1.right = n2
-n2.left = n3
+# n2 = TreeNode.new(2)
+# n3 = TreeNode.new(3)
+# n1.left = n2
+# n2.left = n3
 
 def inorder_traversal(root)
-  
+  result = []
+  if root == nil
+    return result
+  end
+  if root.left
+    result += inorder_traversal(root.left)
+  end
+  result.push(root.val)
+  if root.right
+    result += inorder_traversal(root.right)
+  end
+  return result
 end
 
-p inorder_traversal(n1)
+p inorder_traversal(nil)
