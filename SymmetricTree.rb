25bc6a9f01d068193210c7372f0045e4a28e4771
a-kit-2 <akito.nakamura.c@gmail.com>
2021-06-19 21:32:24 +0900

    準備

---
+++ SymmetricTree.rb
@@ -0,0 +1,33 @@
+# 二分木の根が与えられたら、それがそれ自体の鏡であるかどうかを確認します
+# （つまり、その中心を中心に対称になります）。
+# Input: root = [1,2,2,3,4,4,3]
+# Output: true
+
+class TreeNode
+  attr_accessor :val, :left, :right
+  def initialize(val = 0, left = nil, right = nil)
+    @val = val
+    @left = left
+    @right = right
+  end
+end
+
+t1 = TreeNode.new(1)
+t2 = TreeNode.new(2)
+t3 = TreeNode.new(3)
+t4 = TreeNode.new(4)
+t5 = TreeNode.new(2)
+t6 = TreeNode.new(4)
+t7 = TreeNode.new(3)
+t1.left = t2
+t2.left = t3
+t2.right = t4
+t1.right = t5
+t5.left = t6
+t5.right = t7
+
+def is_symmetric(root)
+  
+end
+
+p is_symmetric(t1)
