0298ed67b864eb215ae8bf8e9eb2e19a046d30e7
a-kit-2 <akito.nakamura.c@gmail.com>
2021-06-20 17:48:20 +0900

    準備

--- LongestSubstringWithoutRepeatingCharacters.rb
+++ LongestSubstringWithoutRepeatingCharacters.rb
@@ -5,7 +5,29 @@
 # Explanation: The answer is "abc", with the length of 3.
 
 def length_of_longest_substring(s)
+  len = s.length
+  return len if len <= 1
 
+  max = 1
+  idx = 1
+  str = s[0]
+  while idx < len
+    if str.include?(s[idx])
+      str = update_str(str, s[idx])
+      str += s[idx]
+    else
+      str += s[idx]
+      max = str.length if str.length > max
+    end
+    idx += 1
+  end
+
+  max
+end
+
+def update_str(str, ch)
+  idx = str.index(ch)
+  str[idx + 1..-1]
 end
 
 p length_of_longest_substring("abcabcbb")
