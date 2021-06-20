b074268925bfdffe61fa2d86a34127695d2e732a
a-kit-2 <akito.nakamura.c@gmail.com>
2021-06-20 17:40:24 +0900

    準備

---
+++ LongestSubstringWithoutRepeatingCharacters.rb
@@ -0,0 +1,11 @@
+# 文字列sが与えられた場合、
+# 文字を繰り返さずに最長の部分文字列の長さを見つけます。
+# Input: s = "abcabcbb"
+# Output: 3
+# Explanation: The answer is "abc", with the length of 3.
+
+def length_of_longest_substring(s)
+
+end
+
+p length_of_longest_substring("abcabcbb")
