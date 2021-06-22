49c8148266ca89589e8eb5abd0762b205f539135
a-kit-2 <akito.nakamura.c@gmail.com>
2021-06-22 19:02:31 +0900

    準備

---
+++ LongestPalindromicSubstring.rb
@@ -0,0 +1,10 @@
+# 文字列sが与えられた場合、sで最も長い回文部分文字列を返します。
+# Input: s = "babad"
+# Output: "bab"
+# Note: "aba" is also a valid answer.
+
+def longest_palindrome(s)
+
+end
+
+p longest_palindrome("babad")
