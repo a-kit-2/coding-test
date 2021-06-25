d5812d8297f0da8c6cea8cc0129da6719fe45a52
a-kit-2 <akito.nakamura.c@gmail.com>
2021-06-25 19:33:03 +0900

    SecondHighestSalary

--- SecondHighestSalary.sql
+++ SecondHighestSalary.sql
@@ -20,3 +20,6 @@ Truncate table Employee;
 insert into Employee (Id, Salary) values ('1', '100');
 insert into Employee (Id, Salary) values ('2', '200');
 insert into Employee (Id, Salary) values ('3', '300');
+
+select max(Salary) as SecondHighestSalary from Employee
+where Salary < (select max(Salary) from Employee);
