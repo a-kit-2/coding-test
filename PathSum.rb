97c559292618d3da6e59d5d378c0cd8176cf8032
a-kit-2 <akito.nakamura.c@gmail.com>
2021-06-29 17:28:01 +0900

    PathSum

--- PathSum.rb
+++ PathSum.rb
@@ -7,12 +7,12 @@
 
 # Definition for a binary tree node.
 class TreeNode
-    attr_accessor :val, :left, :right
-    def initialize(val = 0, left = nil, right = nil)
-        @val = val
-        @left = left
-        @right = right
-    end
+  attr_accessor :val, :left, :right
+  def initialize(val = 0, left = nil, right = nil)
+    @val = val
+    @left = left
+    @right = right
+  end
 end
 # @param {TreeNode} root
 # @param {Integer} target_sum
