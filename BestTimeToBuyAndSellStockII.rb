822ca61fe48e36b8906ad2b485cdf2c8da034f30
a-kit-2 <akito.nakamura.c@gmail.com>
2021-07-05 20:10:27 +0900

    BestTimeToBuyAndSellStockII

--- BestTimeToBuyAndSellStockII.rb
+++ BestTimeToBuyAndSellStockII.rb
@@ -11,7 +11,13 @@
 # @param {Integer[]} prices
 # @return {Integer}
 def max_profit(prices)
-  
+  profit = 0
+  (1...prices.length).each do |i|
+    if prices[i] > prices[i - 1]
+      profit += prices[i] - prices[i - 1]
+    end
+  end
+  return profit
 end
 
 require 'minitest/autorun'
