d03e621fb2bb5989cef5093018d7df87792c9c27
a-kit-2 <akito.nakamura.c@gmail.com>
2021-07-06 22:02:59 +0900

    ValidPalindrome

--- ValidPalindrome.rb
+++ ValidPalindrome.rb
@@ -1,10 +1,16 @@
 # 文字列sが与えられた場合、英数字のみを考慮し、大文字と小文字を無視して、それが回文であるかどうかを判別します。
-# 入力：s = "A man, a plan, a canal: Panama" 出力：true 説明：「amanaplanacanalpanama」は回文です。
+# Input:s = "A man, a plan, a canal: Panama" 
+# Output:true 説明：「amanaplanacanalpanama」は回文です。
 
 # @param {String} s
 # @return {Boolean}
 def is_palindrome(s)
-  
+  s = s.gsub(/[^a-zA-Z0-9]/,"")
+  if s.downcase == s.downcase.reverse
+    return true
+  else
+    return false
+  end
 end
 
 require 'minitest/autorun'
