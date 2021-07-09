86b5ac6e75f02112b1f50c38bb246dfe5ceae770
a-kit-2 <akito.nakamura.c@gmail.com>
2021-07-09 23:58:49 +0900

    Single Number

--- SingleNumber.rb
+++ SingleNumber.rb
@@ -6,7 +6,11 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def single_number(nums)
-  
+  result = 0
+  nums.each do |num|
+    result ^= num
+  end
+  return result
 end
 
 require 'minitest/autorun'
