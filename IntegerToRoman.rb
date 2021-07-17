3a27533250b0374801f70246142acd362c0ac1f6
a-kit-2 <akito.nakamura.c@gmail.com>
2021-07-17 21:51:23 +0900

    IntegerToRoman

--- IntegerToRoman.rb
+++ IntegerToRoman.rb
@@ -25,7 +25,16 @@
 # @param {Integer} num
 # @return {String}
 def int_to_roman(num)
-  
+  integer = [1000, 900, 500, 400, 100,  90,  50,  40,  10,   9,   5,   4,   1]
+  roman   = [ "M","CM", "D","CD", "C","XC", "L","XL", "X","IX", "V", "IV","I"]
+  result = ""
+  integer.length.times do |i|
+    while num >= integer[i]
+      result += roman[i]
+      num -= integer[i]
+    end
+  end
+  return result
 end
 
 require 'minitest/autorun'
