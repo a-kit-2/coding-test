7ebf9af6e2ec97a43cdd0af92935a287aedd8a26
a-kit-2 <akito.nakamura.c@gmail.com>
2021-07-18 21:05:22 +0900

    3Sum

---
+++ 3Sum.rb
@@ -0,0 +1,22 @@
+# 整数配列numsが与えられた場合、
+# i！= j、i！= k、j！= k、およびnums[i]+nums[j]+nums [k] == 0となるように、
+# すべてのトリプレット[nums [i]、nums [j]、nums [k]]を返します。
+# ソリューションセットに重複するトリプレットが含まれていてはならないことに注意してください。
+# Input: nums = [-1,0,1,2,-1,-4]
+# Output: [[-1,-1,2],[-1,0,1]]
+
+# @param {Integer[]} nums
+# @return {Integer[][]}
+def three_sum(nums)
+  
+end
+
+require 'minitest/autorun'
+
+class 3SumTest < Minitest::Test
+  def test_three_sum
+    assert_equal [[-1,-1,2],[-1,0,1]], three_sum([-1,0,1,2,-1,-4])
+    assert_equal [], three_sum([])
+    assert_equal [], three_sum([0])
+  end
+end
