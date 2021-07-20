a203e2c8a9f073818576478f798e0a4ab5748178
a-kit-2 <akito.nakamura.c@gmail.com>
2021-07-20 23:46:53 +0900

    Letter Combinations of a Phone Number

--- LetterCombinationsOfaPhoneNumber.rb
+++ LetterCombinationsOfaPhoneNumber.rb
@@ -7,7 +7,33 @@
 # @param {String} digits
 # @return {String[]}
 def letter_combinations(digits)
-  
+  if digits.length == 0
+    return []
+  end
+  return search(0, "", digits)
+end
+
+def search(index, str, digits)
+  phone = {
+    "2" => "abc",
+    "3" => "def",
+    "4" => "ghi",
+    "5" => "jkl",
+    "6" => "mno",
+    "7" => "pqrs",
+    "8" => "tuv",
+    "9" => "wxyz",
+  }
+  result = []
+  if index == digits.length
+    result.push(str)
+  else
+    letters = phone[digits[index]]
+    letters.length.times do |i|
+      result += search(index + 1, str + letters[i], digits)
+    end
+  end
+  return result
 end
 
 require 'minitest/autorun'
