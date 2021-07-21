f9680c186fca9d941e9f31ce2920d3c0a95b5a83
a-kit-2 <akito.nakamura.c@gmail.com>
2021-07-22 00:19:45 +0900

    ClassesMoreThan5Students

--- ClassesMoreThan5Students.sql
+++ ClassesMoreThan5Students.sql
@@ -30,7 +30,10 @@ insert into courses (student, class) values ('D', 'Biology');
 insert into courses (student, class) values ('E', 'Math');
 insert into courses (student, class) values ('F', 'Computer');
 insert into courses (student, class) values ('G', 'Math');
-insert into courses (student, class) values ('H', 'Math');
-insert into courses (student, class) values ('I', 'Math');
+-- insert into courses (student, class) values ('H', 'Math');
+-- insert into courses (student, class) values ('I', 'Math');
+insert into courses (student, class) values ('A', 'Math');
 
-select * from courses;
+select class from courses
+group by class
+having count(distinct student) >= 5;
