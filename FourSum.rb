c0b4637badfe3055ccd165e71be555ed5bc562b3
a-kit-2 <akito.nakamura.c@gmail.com>
2021-07-22 18:18:09 +0900

    FourSum

--- FourSum.rb
+++ FourSum.rb
@@ -1,6 +1,6 @@
 # n個の整数の配列numsが与えられた場合、
 # 次のように、すべての一意の4つ組[nums [a]、nums [b]、nums [c]、nums [d]]の配列を返します。
-# ・0 <= a、b、c、d <n 
+# ・0 <= a、b、c、d < n 
 # ・a、b、c、およびdは区別されます。 
 # ・nums [a] + nums [b] + nums [c] + nums [d] == ターゲット
 # Input: nums = [1,0,-1,0,-2,2], target = 0
