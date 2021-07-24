50a79bb8573d16e57b560d4868eec32a2bad8303
a-kit-2 <akito.nakamura.c@gmail.com>
2021-07-24 19:19:35 +0900

    Generate Parentheses

--- GenerateParentheses.rb
+++ GenerateParentheses.rb
@@ -5,7 +5,21 @@
 # @param {Integer} n
 # @return {String[]}
 def generate_parenthesis(n)
-  
+  result = []
+  generate(result, "", 0, 0, n)
+  return result
+end
+
+def generate(result, current, left_count, right_count, max)
+  if left_count + right_count == max * 2
+    result.push(current)
+  end
+  if left_count < max
+    generate(result, current + "(", left_count + 1, right_count, max)
+  end
+  if right_count < left_count
+    generate(result, current + ")", left_count, right_count + 1, max)
+  end
 end
 
 require 'minitest/autorun'
