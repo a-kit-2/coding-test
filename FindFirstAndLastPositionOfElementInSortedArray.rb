42a6c5fcd490433a1e63729f2819fda1e6a7b9ca
a-kit-2 <akito.nakamura.c@gmail.com>
2021-08-02 15:46:28 +0900

    FindFirstAndLastPositionOfElementInSortedArray

--- FindFirstAndLastPositionOfElementInSortedArray.rb
+++ FindFirstAndLastPositionOfElementInSortedArray.rb
@@ -1,6 +1,9 @@
-昇順でソートされた整数numの配列が与えられた場合、与えられたターゲット値の開始位置と終了位置を見つけます。 配列内にターゲットが見つからない場合は、[-1、-1]を返します。 実行時の複雑さがO（log n）のアルゴリズムを作成する必要があります。
-Input: nums = [5,7,7,8,8,10], target = 8
-Output: [3,4]
+# 昇順でソートされた整数numの配列が与えられた場合、
+# 与えられたターゲット値の開始位置と終了位置を見つけます。 
+# 配列内にターゲットが見つからない場合は、[-1、-1]を返します。 
+# 実行時の複雑さがO（log n）のアルゴリズムを作成する必要があります。
+# Input: nums = [5,7,7,8,8,10], target = 8
+# Output: [3,4]
 
 # @param {Integer[]} nums
 # @param {Integer} target
@@ -11,8 +14,8 @@ end
 
 require 'minitest/autorun'
 
-class SearchInRotatedSortedArrayTest < Minitest::Test
-  def test_search
-    assert_equal [1,3,2], search()
+class FindFirstAndLastPositionOfElementInSortedArrayTest < Minitest::Test
+  def test_search_range
+    assert_equal [1,3,2], search_range()
   end
 end
