f031b03603890478ad633c3973f78f9400b29d65
a-kit-2 <akito.nakamura.c@gmail.com>
2021-08-24 23:13:42 +0900

    MajorityElement

---
+++ MajorityElement.rb
@@ -0,0 +1,19 @@
+# サイズnの配列numsが与えられた場合、多数決要素を返します。
+# 多数決要素は、⌊n/2⌋回以上出現する要素です。
+# 多数決要素は常に配列に存在すると想定できます。
+# Input: nums = [3,2,3]
+# Output: 3
+
+# @param {Integer[]} nums
+# @return {Integer}
+def majority_element(nums)
+  
+end
+
+require 'minitest/autorun'
+
+class MajorityElementTest < Minitest::Test
+  def test_majority_element
+    assert_equal 3, majority_element([3,2,3])
+  end
+end
