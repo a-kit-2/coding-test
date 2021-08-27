714e83a1a8f23c731816e0eaa4e5bce2d4364893
a-kit-2 <akito.nakamura.c@gmail.com>
2021-08-27 23:28:37 +0900

    MajorityElement

--- MajorityElement.rb
+++ MajorityElement.rb
@@ -7,7 +7,15 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def majority_element(nums)
-  
+  # length:7
+  #  ----------
+  # [1, 1, 1, 2, 2, 2, 2]
+  #           ----------
+
+  # length:8
+  # --------------
+  # [1, 1, 1, 2 ,2, 2, 2, 2]
+  #           -------------
 end
 
 require 'minitest/autorun'
@@ -16,5 +24,6 @@ class MajorityElementTest < Minitest::Test
   def test_majority_element
     assert_equal 3, majority_element([3,2,3])
     assert_equal 2, majority_element([2,2,1,1,1,2,2])
+    assert_equal 5, majority_element([4,5,4,5,4,5,5,5])
   end
 end
